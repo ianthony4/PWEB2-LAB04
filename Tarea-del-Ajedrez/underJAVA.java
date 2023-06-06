994d9ae962f91d5563a913ce91210bef2e3b5f15
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-06 01:29:12 -0500

    Creando el archivo java, que contiene la funcion 'UNDER' pero resuelto en ese mismo lenguaje

---
+++ Tarea-del-Ajedrez/underJAVA.java
@@ -0,0 +1,10 @@
+//1. Esta clase representa una solucion en java a la funcion under de python
+//2. Esto se hizo a modo de practica
+//3. Este archivo cuenta como evidencia del desarrollo de la funcion para
+//   apicarlo en python sea mas sencillo
+
+//Autor:  Anthony Chaisa Fernandez
+import java.util.ArrayList;
+public class underJAVA{
+
+}
