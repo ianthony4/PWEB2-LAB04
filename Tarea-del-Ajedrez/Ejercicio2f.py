74185367fe02042cc2d90f12ce9242de5b7e7936
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-07 01:23:25 -0500

    'EJ 2-F' creando la tira de casilleros, igual a la del ejercicio E y/o D

--- Tarea-del-Ajedrez/Ejercicio2f.py
+++ Tarea-del-Ajedrez/Ejercicio2f.py
@@ -3,3 +3,5 @@ from chessPictures import *
 #Modulo base
 #Square negro y blanco
 moduloBase = square.negative().join(square)
+#Tira de casilleros (1 linea)
+tira1Linea = moduloBase.horizontalRepeat(4)
