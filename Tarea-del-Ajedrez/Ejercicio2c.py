7a89b33358a9f2878dc616460ae3d3993db3446e
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-07 00:07:56 -0500

    'EJ 2-C' SOLUCIONADO, comentarios dentro del script

--- Tarea-del-Ajedrez/Ejercicio2c.py
+++ Tarea-del-Ajedrez/Ejercicio2c.py
@@ -1,2 +1,7 @@
 from interpreter import draw
 from chessPictures import *
+#El modulo (En este caso solo es la REINA como tal)
+#Salida
+output = queen.horizontalRepeat(4)
+#Dibujamos la salida
+draw (output)
