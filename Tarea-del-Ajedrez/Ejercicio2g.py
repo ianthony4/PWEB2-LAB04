b4d59e53e8e34a50bdceb5841591d5e26df994df
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-07 02:20:26 -0500

    'EJ 2-G' EXPLICACION DE INTENTOS ANTERIORES

--- Tarea-del-Ajedrez/Ejercicio2g.py
+++ Tarea-del-Ajedrez/Ejercicio2g.py
@@ -1,5 +1,15 @@
 from interpreter import draw
 from chessPictures import *
+
+"""Antes de llegar a la solucion de abajo, se estuvo intentanto
+    metodos como crear una linea de figuras y casilleros, usando 'join' """
+# filaFiguras = rock.join(knight).join(bishop).........................join(rock)
+# filaCasilleros = square.join(square.negative()).horizontalRepeat(4)
+"""Y luego hacer uso de la funcion UNDER, como si fueran 2 figuras"""
+# firstLine = filaCasilleros.under(filaFiguras)
+# draw(firstLine)
+"""Lo anterior no funciono, debido a esto que decidimos trabajar FIGURA POR FIGURA para under"""
+
 #Modulo de Figuras
 figuras = [rock,knight,bishop,queen,king,bishop,knight,rock]
 # Creacion de la primera linea
