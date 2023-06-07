b9a04b13662cc4210fa69e0740bdb9e97f2b5900
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-07 00:05:14 -0500

    'EJ 2-B' SOLUCIONADO, detalles dentro del script

--- Tarea-del-Ajedrez/Ejercicio2b.py
+++ Tarea-del-Ajedrez/Ejercicio2b.py
@@ -1,2 +1,10 @@
 from interpreter import draw
 from chessPictures import *
+
+#El modulo
+#Caballo negro y caballo blanco juntos
+kniBkniW = knight.negative().join(knight)
+#La salida
+output = (kniBkniW.horizontalMirror().join(knight.horizontalMirror())).up(kniBkniW.negative())
+#Dibujar la salida
+draw(output)
