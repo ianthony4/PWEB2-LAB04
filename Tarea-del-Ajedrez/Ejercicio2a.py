3e8ec39e0a762c0438ad0cd40cdc389c3871de46
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-06 23:51:18 -0500

    'EJ 2-A' resolviendo el ejercicio, instrucciones a detalle en los comentaios python

--- Tarea-del-Ajedrez/Ejercicio2a.py
+++ Tarea-del-Ajedrez/Ejercicio2a.py
@@ -1,2 +1,13 @@
 from interpreter import draw
 from chessPictures import *
+
+# Tan sencillo como combinar una serie de metodos
+# que nosotros mismos hemos creado ;)  
+
+#Caballo Negro y caballo blanco juntos
+kniBkniW = knight.negative().join(knight)
+#La salida
+output = (kniBkniW.up((kniBkniW).negative()))
+#Dibujar salida
+draw(output)
+
