3aa735e647f5f1c48b3fe1f9e00759e4e87e85f0
Anthony Chaisa <achaisa@unsa.edu.pe>
2023-06-07 01:25:00 -0500

    'EJ 2-F' creando la tira de casilleros de 2 filas

--- Tarea-del-Ajedrez/Ejercicio2f.py
+++ Tarea-del-Ajedrez/Ejercicio2f.py
@@ -5,3 +5,5 @@ from chessPictures import *
 moduloBase = square.negative().join(square)
 #Tira de casilleros (1 linea)
 tira1Linea = moduloBase.horizontalRepeat(4)
+#Tira de casilleros (2 lineas, linea de arriba invertida)
+tira2Linea = tira1Linea.up(tira1Linea.negative())
